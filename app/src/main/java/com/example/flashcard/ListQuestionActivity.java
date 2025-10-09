0ec69d1e021d6485bff3894d6991d962a928b9cc
SB <swannbuissieres@gmail.com>
2025-10-09 10:07:48 +0200

    update recyclerView
    base working, missing customs items

--- app/src/main/java/com/example/flashcard/ListQuestionActivity.java
+++ app/src/main/java/com/example/flashcard/ListQuestionActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,6 +16,7 @@ import java.util.Arrays;
 public class ListQuestionActivity extends AppCompatActivity {
 
     private ArrayList<Question> questions;
+    private ListQuestionAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +24,17 @@ public class ListQuestionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_question);
         questions = new ArrayList<>();
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 30; i++) {
             questions.add(new Question("Qui est Novak Djokovic ?", Arrays.asList("Un joueur de tennis", "Un footballeur", "Un nageur", "Un rappeur"), 0,"facile","nan.mp3"));
 
         }
+
+        adapter = new ListQuestionAdapter(questions);
+
+        RecyclerView recyclerView = findViewById(R.id.ListeQuestions);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
